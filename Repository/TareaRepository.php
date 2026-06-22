eed2b74c82257b4c22cb2e0ace9fbf995a452095
Sibunaq34 <105940866+Sibunaq34@users.noreply.github.com>
2026-06-22 11:17:10 -0600

    Estructura y avance 1

---
+++ Repository/TareaRepository.php
@@ -0,0 +1,130 @@
+<?php
+
+class TareaRepository
+{
+
+    private PDO $conexion;
+
+
+    public function __construct(PDO $conexion)
+    {
+        $this->conexion = $conexion;
+    }
+
+
+
+    public function listar()
+    {
+
+        $sql = "CALL ListarTareas()";
+
+        $stmt = $this->conexion->prepare($sql);
+
+        $stmt->execute();
+
+
+        return $stmt->fetchAll(
+            PDO::FETCH_ASSOC
+        );
+
+    }
+
+
+
+    public function obtenerPorId($id)
+    {
+
+        $sql = "CALL ObtenerTareaPorId(?)";
+
+
+        $stmt = $this->conexion->prepare($sql);
+
+
+        $stmt->execute([
+            $id
+        ]);
+
+
+        return $stmt->fetch(
+            PDO::FETCH_ASSOC
+        );
+
+    }
+
+
+
+    public function crear(Tarea $tarea)
+    {
+
+        $sql = "
+        CALL CrearTarea(?,?,?,?)
+        ";
+
+
+        $stmt = $this->conexion->prepare($sql);
+
+
+        $stmt->execute([
+
+            $tarea->detalle,
+
+            $tarea->prioridad,
+
+            $tarea->fechaLimite,
+
+            $tarea->idResponsable
+
+        ]);
+
+    }
+
+
+
+
+    public function editar(Tarea $tarea)
+    {
+
+        $sql = "
+        CALL EditarTarea(?,?,?,?,?)
+        ";
+
+
+        $stmt = $this->conexion->prepare($sql);
+
+
+        $stmt->execute([
+
+            $tarea->idTarea,
+
+            $tarea->detalle,
+
+            $tarea->prioridad,
+
+            $tarea->fechaLimite,
+
+            $tarea->idResponsable
+
+        ]);
+
+    }
+
+
+
+    public function eliminar($id)
+    {
+
+        $sql = "
+        CALL EliminarTarea(?)
+        ";
+
+
+        $stmt = $this->conexion->prepare($sql);
+
+
+        $stmt->execute([
+            $id
+        ]);
+
+    }
+
+}
